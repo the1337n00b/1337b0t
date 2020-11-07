a5a556bc6d5865e676b6d06e904ed2037d86244e
Keith Steckert <ksteckert@gmail.com>
2020-11-07 07:17:42 -0500

    digital ocean server handler

--- config.js
+++ config.js
@@ -8,6 +8,9 @@ try {
 // if the creds aren't found in config.json we'll check for ENV VARS
 var config = JSON.parse(JSON.stringify(config_json))
 if (!config.PG_CONN) config.PG_CONN = process.env.PG_CONN
+if (!config.DO_TOKEN) config.DO_TOKEN = process.env.DO_TOKEN
+if (!config.DO_DOMAIN) config.DO_DOMAIN = process.env.DO_DOMAIN
+if (!config.RCON_PASS) config.RCON_PASS = process.env.RCON_PASS
 if (!config.BOT_TOKEN) config.BOT_TOKEN = process.env.BOT_TOKEN
 
 module.exports = config
